273457faf1f2b674cfd6b64798eabd43da35c76b
Stuart Campbell <stuart@harto.org>
2013-01-28 14:51:01 +1100

    WIP capture and normalise platform key events

--- swing/src/enoki/swing.clj
+++ swing/src/enoki/swing.clj
@@ -1,8 +1,35 @@
 (ns enoki.swing
-  (:require [enoki.graphics.java2d :as gfx]))
+  (:require [clojure.string :as str]
+            [seesaw.core :as seesaw]
+            [enoki.graphics.java2d :as gfx]
+            [enoki.keyboard :as kbd]
+            [enoki.util.logging]
+            [enoki.util.logging-macros :as log])
+  (:import [java.awt.event KeyEvent]))
 
-(defn- create-frame []
+;; Interpret native key events
+
+(defn event-key
+  "Transform a KeyEvent into a keyword describing the key being pressed or
+   released."
+  [e]
+  ;; FIXME: this obviously needs to be much smarter.
+  ;; It only currently only handles alphanumeric keys.
+  (-> (.getKeyCode e)
+      (KeyEvent/getKeyText)
+      (str/lower-case)
+      (keyword)))
+
+(defn key-pressed [e]
+  (kbd/enqueue-event! :key-pressed (event-key e)))
+
+(defn key-released [e]
+  (kbd/enqueue-event! :key-released (event-key e)))
+
+(defn create-frame []
   (doto (seesaw/frame :resizable? false)
+    (seesaw/listen :key-pressed #'key-pressed)
+    (seesaw/listen :key-released #'key-released)
     (.setIgnoreRepaint true)))
 
 (defn env
--- web/src/cljs/enoki/web.cljs
+++ web/src/cljs/enoki/web.cljs
@@ -1,8 +1,25 @@
 (ns enoki.web
   (:require [clojure.browser.repl :as repl]
             [enoki.graphics.canvas :as gfx]
-            [enoki.util.logging :as logging]
-            ))
+            [enoki.keyboard :as kbd]
+            [enoki.util.logging :as logging])
+  (:require-macros [enoki.util.logging-macros :as log])
+            )
+
+(defn- key-pressed [e]
+  (log/info "key-pressed: %s" (.-keyCode e))
+  (.preventDefault e)
+  )
+
+(defn- key-released [e]
+  (log/info "key-released: %s" (.-keyCode e))
+  (.preventDefault e)
+  )
+
+(defn capture-key-events! [element]
+  (doto element
+    (.addEventListener "keypress" key-pressed)
+    (.addEventListener "keyup" key-released)))
 
 (defn init! []
   (repl/connect "http://localhost:9000/repl")
@@ -12,4 +29,5 @@
 (defn env
   "Initialise and return a web-based environment."
   [canvas-element]
+  (capture-key-events! js/window)
   {:display (gfx/->CanvasDisplay canvas-element)})
