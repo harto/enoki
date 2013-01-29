095f005bd84f57233e2877af5a81d5df4d57d987
Stuart Campbell <stuart@harto.org>
2013-01-29 19:38:49 +1100

    Remove a bunch of unused logging cruft

--- web/src/cljs/enoki/logging.cljs
+++ web/src/cljs/enoki/logging.cljs
@@ -22,26 +22,6 @@
      (if-let [level (levels level)]
        (.setLevel logger level))))
 
-;; ## Logging handlers
-
-;; (def handlers (atom nil))
-
-;; (defn add-handler
-;;   "Register a logging handler of the form
-;;    (fn [level logger-name time message])"
-;;   [f]
-;;   (swap! handlers conj f))
-
-;; (defn- handler-delegate
-;;   "Unpack a goog.debug.LogRecord and send it to all handler functions."
-;;   [record]
-;;   (let [level (.getLevel record)
-;;         name (.getLoggerName record)
-;;         time (.getMillis record)
-;;         message (.getMessage record)]
-;;     (doseq [handler @handlers]
-;;       (handler level name time message))))
-
 ;; ## Logging functions
 
 (defn log* [logger-name level message-fn]
@@ -53,6 +33,4 @@
 ;; ## Initialisation
 
 (defn init! []
-  (.setCapturing (goog.debug.Console.) true)
-  ;; (.addHandler (root-logger) handler-delegate)
-  )
+  (.setCapturing (goog.debug.Console.) true))
