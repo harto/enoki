2ca318f1802bc0902d0571cda3089cc045131bfe
Stuart Campbell <stuart@harto.org>
2013-09-21 18:50:12 +1000

    Don't share keycodes across environments

--- swing/src/enoki/swing.clj
+++ swing/src/enoki/swing.clj
@@ -7,9 +7,40 @@
             [enoki.logging-macros :as log])
   (:import [java.awt.event KeyEvent]))
 
+(defn- map-char-range [from-char to-char]
+  (into {} (for [code (range (int from-char)
+                             (inc (int to-char)))
+                 :let [identifier (-> (char code) (str/lower-case) (keyword))]]
+             [code identifier])))
+
 (def ^:private key-names
-  (merge kbd/default-key-names
-         {10 :enter
+  "Map keycodes to keynames."
+  (merge (map-char-range \A \Z)
+         (map-char-range \0 \9)
+         ;; Function keys (F1-F12)
+         (zipmap (range 112 124)
+                 (for [i (range 12)] (keyword (str "f" (inc i)))))
+         ;; FIXME: The commented-out mappings are only available on a full-sized
+         ;; keyboard, and thus haven't been tested yet.
+         ;; Also TODO: numpad keys.
+         {8 :backspace
+          9 :tab
+          10 :enter
+          16 :shift
+          17 :control
+          18 :alt
+          ;;19 :pause
+          ;;20 :caps-lock
+          27 :escape
+          32 :space
+          ;;33 :page-up
+          ;;34 :page-down
+          ;;35 :end
+          ;;36 :home
+          37 :left
+          38 :up
+          39 :right
+          40 :down
           44 :comma
           45 :minus
           46 :period
@@ -19,7 +50,11 @@
           91 :open-bracket
           92 :backslash
           93 :close-bracket
-          157 :meta}))
+          ;;144 :num-lock
+          ;;145 :scroll-lock
+          157 :meta
+          192 :backquote
+          222 :quote}))
 
 (defn- handle-key-event [event-type e]
   (if-let [key-name (get key-names (.getKeyCode e))]
--- web/src/cljs/enoki/web.cljs
+++ web/src/cljs/enoki/web.cljs
@@ -1,28 +1,74 @@
 (ns enoki.web
   (:require [clojure.browser.repl :as repl]
+            [clojure.string :as str]
+            [enoki.event :as event]
             [enoki.graphics.canvas :as gfx]
             [enoki.keyboard :as kbd]
             [enoki.logging :as logging])
   (:require-macros [enoki.logging-macros :as log]))
 
+;; We have to deal in strings, not chars, since ClojureScript doesn't have
+;; character literals or a (char) function.
+
+(defn char->int [s]
+  (.charCodeAt s 0))
+
+(defn int->char [i]
+  (.fromCharCode js/String i))
+
+(defn map-char-range [from-char to-char]
+  (into {} (for [code (range (char->int from-char)
+                             (inc (char->int to-char)))
+                 :let [identifier (-> (int->char code) (str/lower-case) (keyword))]]
+             [code identifier])))
+
 (def key-names
-  (merge kbd/default-key-names
-         {13 :enter
-          ;45 :insert
+  (merge (def default-key-names
+  "Platform-independent mappings of keycodes to keynames."
+  (merge (map-char-range "A" "Z")
+         (map-char-range "0" "9")
+         ;; Function keys (F1-F12)
+         (zipmap (range 112 124) (for [i (range 12)] (keyword (str "f" (inc i)))))
+         ;; FIXME: The commented-out mappings are only available on a full-sized
+         ;; keyboard, and thus haven't been tested yet.
+         ;; Also TODO: numpad keys.
+         {8 :backspace
+          9 :tab
+          13 :enter
+          16 :shift
+          17 :control
+          18 :alt
+          ;;19 :pause
+          ;;20 :caps-lock
+          27 :escape
+          32 :space
+          ;;33 :page-up
+          ;;34 :page-down
+          ;;35 :end
+          ;;36 :home
+          37 :left
+          38 :up
+          39 :right
+          40 :down
+          ;;45 :insert
           46 :delete
           91 :meta
+          ;;144 :num-lock
+          ;;145 :scroll-lock
           188 :comma
           190 :period
           191 :slash
+          192 :backquote
           ;; FIXME: Opera maps 219 to :meta
           219 :open-bracket
           220 :backslash
           221 :close-bracket
+          222 :quote
           ;; Gloss over browser incompatibilities. See
           ;; http://www.javascripter.net/faq/keycodes.htm
           59 :semicolon, 186 :semicolon
           61 :equals, 107 :equals, 187 :equals
-          109 :minus, 189 :minus}))
+          109 :minus, 189 :minus}))))
 
 (defn handle-key-event [event-type e]
   (if-let [key-name (get key-names (.-keyCode e))]
